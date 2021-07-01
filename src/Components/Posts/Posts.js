604c520ff4658434f211966b6d681a4adbefce44
Sanskar Gupta <72657115+sanskar0901@users.noreply.github.com>
2021-07-01 22:02:45 +0530

    fixes

--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -1,11 +1,38 @@
-
-import Card from './card'
 import React from 'react'
+import Card from './card'
 import Styles from './Posts.module.css'
 
 
 
+
+
 function Posts() {
+    // const [Post, setPost] = useState([
+    //     {
+    //         profilepic: "img",
+    //         username: "TheDogsLover",
+    //         location: "London",
+    //         postimg: "https://www.thesprucepets.com/thmb/wpN_ZunUaRQAc_WRdAQRxeTbyoc=/4231x2820/filters:fill(auto,1)/adorable-white-pomeranian-puppy-spitz-921029690-5c8be25d46e0fb000172effe.jpg",
+    //         likes: 100,
+    //         caption: "Sooooo Cuteee",
+    //         comments: [
+    //             {
+    //                 username: "thedog",
+    //                 comment: "So cuteeeee"
+    //             },
+    //             {
+    //                 username: "thedog",
+    //                 comment: "So Cutieee ❤❤❤❤❤❤"
+    //             },
+    //             {
+    //                 username: "_the_King",
+    //                 comment: "Too cute ❤️❤️"
+    //             }
+
+    //         ]
+    //     }
+    // ])
+    // console.log(Post);
     return (
         <div className={Styles.majorcontainer}>
 
